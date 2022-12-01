51d2f7b6da388cecabd72ed1cb5205a727985ba1
YohSambre <YohSambre@users.noreply.github.com>
2022-12-01 19:40:45 +0100

    Add files via upload

---
+++ ez_antibackdoor/antibackdoorkill/lua/autorun/client/ez.lua
@@ -0,0 +1,8 @@
+function FuckOff(ply)
+  Msg( "\n" )
+end
+concommand.Add( "cpe_menu", FuckOff ) -- https://steamcommunity.com/sharedfiles/filedetails/?id=1714628992
+concommand.Add( "nomalua_scan", FuckOff ) -- https://github.com/THABBuzzkill/nomalua
+concommand.Add( "braxscan", FuckOff ) -- https://github.com/YohSambre/braxscan
+concommand.Add( "braxscan all 1", FuckOff ) -- ^
+concommand.Add( "gac_checkbackdoors", FuckOff ) -- https://github.com/GlorifiedPig/GlorifiedAnticheat
