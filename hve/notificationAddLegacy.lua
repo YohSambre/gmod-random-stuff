df01f90729bea1c052b8f6e74aa28ccef8717505
YohSambre <YohSambre@users.noreply.github.com>
2023-09-15 17:50:51 +0200

    Update notificationAddLegacy.lua

--- hve/notificationAddLegacy.lua
+++ hve/notificationAddLegacy.lua
@@ -1,8 +1,8 @@
 --[[ I have often seen on hack visuals the use of the function https://wiki.facepunch.com/gmod/notification.AddLegacy.
 so i produced my own personal code, you are free to modify it
 ]]
-local function Go()
-    local shittytexts = {
+timer.Create( "UniqueName1", 0.1, 0, function() -- better hook think result
+        local shittytexts = {
         "Smell my ass",
         "FuckOff",
         "Haax!",
@@ -16,5 +16,4 @@ local function Go()
         NOTIFY_CLEANUP
     }
     notification.AddLegacy( table.Random(shittytexts), table.Random(randomnotify), 3 )
-end
-hook.Add( "Think", "Oops", Go )
+end)
